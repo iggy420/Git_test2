40614b9ccbe6823fb3bfc0f2919484954d71d76a
iggy420 <iikka.j.tauro@gmail.com>
2025-01-27 19:16:51 +0200

    merging

--- new_file.py
+++ new_file.py
@@ -1,9 +1,2 @@
 print("This is a new file")
-<<<<<<< HEAD
 print("some more code")
-=======
-print("we made some changes")
-print("here is some new text")
-print("Even more code")
-print("abcd")
->>>>>>> print-text
